68e6e12475fe53c03fd0cc1e98028b4f88821443
insionCEO <amazingrace8190@gmail.com>
2025-03-17 00:22:42 +0530

    feat: add web analytics

--- ui/src/providers/Web3Provider.tsx
+++ ui/src/providers/Web3Provider.tsx
@@ -4,12 +4,13 @@ import { sepolia } from 'wagmi/chains';
 import { http } from 'viem';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@rainbow-me/rainbowkit/styles.css';
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
 const config = getDefaultConfig({
   appName: 'MemeCoin Launchpad',
-  projectId: 'd1907ce9b4565853d18b3413ce5f7ad2',
+  projectId: process.env.NEXT_PUBLIC_RAINBOW_PROJECT || '',
   chains: [sepolia],
   ssr: true,
   transports: {
@@ -23,6 +24,7 @@ export function Web3Provider({ children }: { children: React.ReactNode }) {
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider theme={darkTheme()} coolMode>
           {children}
+          <Analytics />
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
